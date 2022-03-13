214590e41be5ef9404151312f9611ace0094e600
Onur Arslanturk <onur.arslanturk@gmail.com>
2022-03-13 14:10:44 +0300

    first Commit

--- src/Entry/KdvTutar.java
+++ src/Entry/KdvTutar.java
@@ -5,9 +5,8 @@ public class KdvTutar {
     public static void main(String[] args)
     {
         Scanner inp = new Scanner(System.in);
-        int tutar;
         System.out.print("Tutarı giriniz : ");
-        tutar = inp.nextInt();
+        int tutar = inp.nextInt();
         double kdvli = ((tutar >= 0) && (tutar <= 1000)) ? tutar * 1.18 : tutar * 1.08;
         System.out.println(kdvli);
 
