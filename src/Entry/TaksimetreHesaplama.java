563737064a38196924c10a7be00ab920ea16f948
Onur Arslanturk <onur.arslanturk@gmail.com>
2022-03-13 17:07:47 +0300

    taksimetre hesaplama sorusu

---
+++ src/Entry/TaksimetreHesaplama.java
@@ -0,0 +1,15 @@
+package Entry;
+import java.util.Scanner;
+public class TaksimetreHesaplama {
+    public static void main (String[] args)
+    {
+        Scanner kmIn = new Scanner(System.in);
+        System.out.print("Toplam KM'yi giriniz: ");
+        double km = kmIn.nextInt();
+        km = (20<(km*2.20)) ? (km * 2.20) + 10 : 30;
+        System.out.println("Taksimetre ücreti : "+ km);
+
+
+    }
+
+}
