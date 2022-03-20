a3cc74da079e7949013aad7b160179b9c2e97b77
Onur Arslanturk <onur.arslanturk@gmail.com>
2022-03-20 20:57:25 +0300

    For Döngüsü ile üslü Sayı Hesaplayan Program ödevi

---
+++ src/Entry/UsHesaplama.java
@@ -0,0 +1,22 @@
+package Entry;
+import java.util.Scanner;
+public class UsHesaplama {
+    public static void main(String[] args) {
+        int sayi,us;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Üssü alınacak sayı : ");
+        sayi = input.nextInt();
+        int sonuc =sayi;
+        System.out.print("Üs olacak sayı : ");
+        us = input.nextInt();
+        if (us!=0)
+        {
+            for(int i=1;i<us;i++){
+                sonuc*=sayi;
+            }
+            System.out.println(sayi+ " üssü "+ us +" = "+sonuc);
+        }
+        else System.out.println(sayi+ " üssü "+ us +" = "+1);
+    }
+}
+// www.patika.dev
