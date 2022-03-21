4b88f889b7bcb9f0837b6a5cbcd1d3ff46b3dd86
Onur Arslanturk <onur.arslanturk@gmail.com>
2022-03-21 22:28:25 +0300

    yıldızlar ile elmaz program ödevi www.patika.dev

---
+++ src/Entry/DiamondWstars.java
@@ -0,0 +1,25 @@
+package Entry;
+import java.util.Scanner;
+public class DiamondWstars {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int a =0 ; a <= 16; a++) {
+                for (int j = 0; j < (n - i); j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= (2 * i + 1); k++) {
+                    System.out.print("*");
+                }
+                for (int j = 0; j < (n - i); j++) {
+                    System.out.print(" ");
+                }
+
+            }System.out.println(" ");
+        }
+    }
+
+}
