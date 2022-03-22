2a96b2b94e2a1c3a74704c147b2eae0960e2214e
Onur Arslanturk <onur.arslanturk@gmail.com>
2022-03-22 22:24:39 +0300

    Klavyeden girilen bir sayının mükemmel sayı olup/olmadığını bulan ve sayı mükemmel sayı ise ekrana “mükemmel sayıdır.” değilse “mükemmel sayı değildir.” ifadelerini ekrana yazan programı
    www.patika.dev
    
    https://app.patika.dev/onurarslanturk

---
+++ src/Entry/MukemmelSayi.java
@@ -0,0 +1,30 @@
+package Entry;
+import java.util.Scanner;
+public class MukemmelSayi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sayi=0, sum=0;
+        System.out.print("Kaç sayı sorgulatacaksınız : ");
+        int count = input.nextInt();
+        while (count > 0)
+        {
+            System.out.print("Sayı giriniz : ");
+            sayi = input.nextInt();
+            for (int i = sayi;i>0;i--)
+            {
+                if (sayi%i==0) sum +=i;
+            }
+            if (sayi == (sum/2)) System.out.println(sum+"(tam bölenlerin toplamı)/2 = " + (sum/2) +" girdiğiniz "+sayi+" sayısına eşit olduğu için; " + sayi+" mükemmel bir sayidir.");
+            else System.out.println(sum+"(tam bölenlerin toplamı)/2 = " + (sum/2) +" girdiğiniz "+sayi+" sayısına eşit olmadığı için; " + sayi+" mükemmel bir sayı değildir.");
+            sum =0;
+            count--;
+        }
+    }
+}
+
+
+
+
+
+//https://app.patika.dev/onurarslanturk
+//www.patika.dev
