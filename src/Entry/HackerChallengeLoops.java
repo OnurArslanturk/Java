772e405d93999b1451b8b3eada6d006668ff9c6e
Onur Arslanturk <onur.arslanturk@gmail.com>
2022-03-22 23:03:12 +0300

    HackerChallengeLoops projesi
    www.patika.dev
    
    https://app.patika.dev/onurarslanturk

---
+++ src/Entry/HackerChallengeLoops.java
@@ -0,0 +1,15 @@
+package Entry;
+import java.io.*;
+
+public class HackerChallengeLoops {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int i=1; i<=10;i++)
+        {
+            System.out.println(N+" x "+i+" = "+(N*i));
+        }
+       bufferedReader.close();
+    }
+}
