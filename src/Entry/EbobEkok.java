76923e22ea00b9065eb7728c0d3b3c16ef20804d
Onur Arslanturk <onur.arslanturk@gmail.com>
2022-03-22 21:05:28 +0300

    Java ile iki sayının EBOB ve EKOK değerlerini "While Döngüsü" kullanarak www.patika.dev

--- src/Entry/EbobEkok.java
+++ src/Entry/EbobEkok.java
@@ -2,13 +2,16 @@ package Entry;
 import java.util.Scanner;
 public class EbobEkok {
     public static void main(String[] args) {
-
+        int i , ebob = 1, ekok=1;
         Scanner inp = new Scanner(System.in);
         System.out.print("n1 sayısını giriniz :");
         int n1 = inp.nextInt();
-        int i = n1, ebob = 1, ekok=1;
         System.out.print("n2 sayısını giriniz : ");
         int n2 = inp.nextInt();
+
+        //en küçük sayıyı bulma
+        if (n1>n2) i = n1;
+        else i = n2;
 
         while (i > 1) {
             if (n1 % i == 0 && n2 % i == 0) {
@@ -18,6 +21,6 @@ public class EbobEkok {
             }
             i--;
         }
-        System.out.println(" Ebob : " + ebob + " Ekok : " + ekok);
+        System.out.println(" EBOB : " + ebob + " EKOK : " + ekok);
     }
 }
