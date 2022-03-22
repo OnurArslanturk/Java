f30a7a7b704bce3104c31bab2c6fefdbba4b8289
Onur Arslanturk <onur.arslanturk@gmail.com>
2022-03-22 19:17:14 +0300

    yıldızlar ile elmaz program ödevi www.patika.dev

--- src/Entry/DiamondWstars.java
+++ src/Entry/DiamondWstars.java
@@ -3,23 +3,41 @@ import java.util.Scanner;
 public class DiamondWstars {
     public static void main(String[] args) {
         Scanner klavye = new Scanner(System.in);
-        System.out.println("Bir Sayı Giriniz :");
+        System.out.print("kaç satırlık bir bir elmas olsun? :");
         int n = klavye.nextInt();
 
-        for (int i = 0; i <= n ; i++) {
-            for (int a =0 ; a <= 16; a++) {
-                for (int j = 0; j < (n - i); j++) {
+        System.out.print("kaç adet elmas olsun? :");
+        int diamondQuant= klavye.nextInt();
+
+
+        for (int i = 0; i <= (n-1) ; i++) { // satır döngüsü - Her döngü turunda bir satır atlıyor. {} içindeki en altta ..println(" "); komutu ile satır atlıyor.
+            for (int a =1 ; a <= diamondQuant; a++) {
+                for (int j = 0; j < ((n-1) - i); j++) {
                     System.out.print(" ");
                 }
                 for (int k = 1; k <= (2 * i + 1); k++) {
                     System.out.print("*");
                 }
-                for (int j = 0; j < (n - i); j++) {
+                for (int j = 0; j < ((n-1) - i); j++) {
                     System.out.print(" ");
                 }
+            }
+            System.out.println(" ");
+        }
 
-            }System.out.println(" ");
+        for (int i = 1; i <= (n-1) ; i++) { // n-1 çünkü en alttaki *** satırı en geniş ** bloğu ve bunu üstteki döngüde yaptık. Bunun tekrar etmemesi adına satır sayısı-1 olarak dönüyor
+            for (int a =0 ; a < diamondQuant; a++) { // yan yana elmasların alt yani ters üçgenlerini yapan kısım.
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < (((n*2)-1)-(i*2)); j++) {
+                    System.out.print("*");
+                }
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
         }
     }
-
 }
