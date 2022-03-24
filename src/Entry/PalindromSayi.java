717f08c16d7dccd6eb7c9b0d8d92126fd2385a39
Onur Arslanturk <onur.arslanturk@gmail.com>
2022-03-24 22:29:13 +0300

    Java ile bir sayının "Palindrom Sayı" olup olmadığını bulan program
    www.patika.dev
    
    https://app.patika.dev/onurarslanturk

---
+++ src/Entry/PalindromSayi.java
@@ -0,0 +1,32 @@
+package Entry;
+import java.util.Scanner;
+public class PalindromSayi {
+
+
+    static boolean palindrom(int n) {
+        int temp = n, reverseN = 0, lastN;
+        while( temp != 0){
+
+            lastN = temp % 10;
+            reverseN = (reverseN *10) + lastN;
+            temp /= 10;
+         }
+        if (n == reverseN) return true;
+        else return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Sayı girin : ");
+        int n = in.nextInt();
+        if (palindrom(n)) System.out.println(n + " Sayısı polindrom sayıdır.");
+        else System.out.println( n + " Sayısı polindrom sayı değildir.");
+
+    }
+}
+
+
+
+
+// www.patika.dev
+// https://app.patika.dev/onurarslanturk
